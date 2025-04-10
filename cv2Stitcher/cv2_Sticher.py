693013c6435612f4cb1644f3229007a5cc8c9aa5
Priyabrata004 <priyabratasaha8359@gmail.com>
2025-04-10 23:52:30 +0530

    Update cv2_Sticher.py for showing upload 2 images in app

--- cv2Stitcher/cv2_Sticher.py
+++ cv2Stitcher/cv2_Sticher.py
@@ -7,29 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1p71jwlQqzMwNvk-oolkrMuctTtBUcw7B
 """
 
-'''# Installing required libraries
-import cv2
-import os
-
-# Fetching the images
-input_folder = 'inputs'
-images = []
-
-for i in os.listdir(input_folder):
-    img = cv2.imread(os.path.join(input_folder,i))
-    images.append(img)
-
-# Performing Stitching using cv2.Sticher_create() class
-stitcher = cv2.Stitcher_create()
-_,res= stitcher.stitch(images)
-
-#Scale and Display the resultant image after Stitching
-from google.colab.patches import cv2_imshow
-res_scaled = cv2.resize(res, (800,600))  #Scale to 800x600
-cv2_imshow(res_scaled)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()'''
-
 import streamlit as st
 import cv2
 import numpy as np
@@ -37,7 +14,7 @@ import tempfile
 import os
 
 st.title("Image Stitching with OpenCV")
-st.write("Upload multiple overlapping images to create a panorama.")
+st.write("Upload 2 overlapping images to create a panorama.")
 
 uploaded_files = st.file_uploader("Upload images", accept_multiple_files=True, type=['jpg', 'jpeg', 'png'])
 
